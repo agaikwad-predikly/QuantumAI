b65f9259c88d22bffea03ec076cfc04e22f4610f
agaikwad-predikly <amit.gaikwad@predikly.com>
2017-12-26 10:37:10 +0530

    Job Changes and scheduling

---
+++ QuantumAI.Service/scheduler.py
@@ -0,0 +1,12 @@
+import schedule
+import time
+import service as sr
+def job():
+    sr.perform()
+
+schedule.every().hour.do(job)
+schedule.every().day.at("00:10").do(job)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
